b113d98e0a8ec3b67b32bc40b5b08a3ed290646d
shahadathossain26 <shiimran172372@gmail.com>
2022-11-25 16:15:27 +0600

    banner done

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import banner from '../../../assets/banner.jpg';
+import './Banner.css'
 
 const Banner = () => {
     return (
-        <div>
-            This is banner.
+        <div className='relative mb-16'>
+            <div className='home-banner'>
+                <img className='w-full h-1/2' src={banner} alt="" />
+            </div>
+
+            <div className='absolute top-[30px] lg:top-1/4 left-10 md:left-16 lg:left-24'>
+                <h1 className='text-[30px] md:text-[50px] lg:text-[70px] font-bold text-white'>Find The Best Motorcycle <br /> <span className='text-primary'>For Youself</span></h1>
+            </div>
+
+            <div className='absolute top-[120px] md:top-[200px] lg:top-1/2 left-10 md:left-16 lg:left-24 w-1/2'>
+                <p className='text-white text-[15px] md:text-[24px] lg:text-[30px]'>Over hundreds of brands and tens of thousands of parts
+
+                </p>
+            </div>
+
+            <div className='absolute top-[120px] md:top-[250px] lg:top-[500px] left-10 md:left-16 lg:left-24 mt-20'>
+                <button className="btn btn-primary text-[12px] md:text-[15px] lg:text-xl text-white">Shop Now</button>
+
+            </div>
         </div>
     );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  daisyui: {
+    themes: [
+      {
+        motortheme: {
+          primary: "#E52727"
+        }
+      }
+    ]
+  },
   theme: {
     extend: {},
   },
