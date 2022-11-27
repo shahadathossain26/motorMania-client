b611173e6763c977f6a70f9d314dcb2a04970060
shahadathossain26 <shiimran172372@gmail.com>
2022-11-27 15:39:22 +0600

    jwt added on registration

--- src/Pages/Dashboard/MyOrders/MyOrders.js
+++ src/Pages/Dashboard/MyOrders/MyOrders.js
@@ -7,7 +7,11 @@ const MyOrders = () => {
     const { data: orders = [] } = useQuery({
         queryKey: ['orders', user?.email],
         queryFn: async () => {
-            const res = await fetch(`http://localhost:5000/orders?email=${user?.email}`);
+            const res = await fetch(`http://localhost:5000/orders?email=${user?.email}`, {
+                headers: {
+                    authorization: `bearer ${localStorage.getItem('accessToken')}`
+                }
+            });
             const data = await res.json();
             return data
         }
@@ -15,53 +19,54 @@ const MyOrders = () => {
     })
 
     return (
+        <div>
+            <h2 className='text-2xl md:text-3xl lg:text-4xl text-primary font-bold mt-12 mb-5 ml-5'>My Orders</h2>
+            <div className="overflow-x-auto w-full">
+                <table className="table w-full">
 
-        <div className="overflow-x-auto w-full">
-            <table className="table w-full">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Product</th>
+                            <th>Name</th>
+                            <th>Price</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
 
-                <thead>
-                    <tr>
-                        <th></th>
-                        <th>Product</th>
-                        <th>Name</th>
-                        <th>Price</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-
-                    {
-                        orders.map((order, i) => <tr
-                            key={order._id}
-                        >
-                            <td>{i + 1}</td>
-                            <td>
-                                <div className="flex items-center space-x-3">
-                                    <div className="avatar">
-                                        <div className="mask mask-squircle w-20 h-20">
-                                            <img src={order.image} alt="Avatar Tailwind CSS Component" />
+                        {
+                            orders.map((order, i) => <tr
+                                key={order._id}
+                            >
+                                <td>{i + 1}</td>
+                                <td>
+                                    <div className="flex items-center space-x-3">
+                                        <div className="avatar">
+                                            <div className="mask mask-squircle w-20 h-20">
+                                                <img src={order.image} alt="Avatar Tailwind CSS Component" />
+                                            </div>
                                         </div>
+
                                     </div>
+                                </td>
+                                <td>
+                                    {order.productName}
+                                </td>
+                                <td>{order.price}</td>
+                                <th>
+                                    <button className="btn btn-primary text-white">Pay</button>
+                                </th>
+                            </tr>)
+                        }
 
-                                </div>
-                            </td>
-                            <td>
-                                {order.productName}
-                            </td>
-                            <td>{order.price}</td>
-                            <th>
-                                <button className="btn btn-primary text-white">Pay</button>
-                            </th>
-                        </tr>)
-                    }
-
-                </tbody>
+                    </tbody>
 
 
 
-            </table>
+                </table>
+            </div>
         </div>
-
     );
 };
 
--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -46,8 +46,17 @@ const Register = () => {
         })
             .then(res => res.json)
             .then(data => {
-                console.log(data);
-                navigate('/');
+                getUserToken(email);
+            })
+    }
+    const getUserToken = email => {
+        fetch(`http://localhost:5000/jwt?email=${email}`)
+            .then(res => res.json())
+            .then(data => {
+                if (data.accessToken) {
+                    localStorage.setItem('accessToken', data.accessToken);
+                    navigate('/');
+                }
             })
     }
     return (
